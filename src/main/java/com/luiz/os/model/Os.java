7fd3bd8d1ccfa1fba87e9d283458422449b4a7d9
luizc <luizc@DESKTOP-PLL2PIG>
2022-09-27 10:08:34 -0300

    Created Os model class

---
+++ src/main/java/com/luiz/os/model/Os.java
@@ -0,0 +1,145 @@
+package com.luiz.os.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.luiz.os.enuns.Prioridade;
+import com.luiz.os.enuns.Status;
+
+@Entity
+public class Os {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy HH:MM")
+	private LocalDateTime dataAbertura;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy HH:MM")
+	private LocalDateTime dataFechamento;
+	
+	private Integer prioridade;
+	private String observacoes;
+	private Integer status;
+	
+	@ManyToOne // São n (muitas) Ordens de serviço para 1 técnico
+	@JoinColumn(name = "tecnico_id")
+	private Tecnico tecnico;
+	
+	@ManyToOne // São n (muitas) Ordens de serviço para 1 cliente
+	@JoinColumn(name = "cliente_id")
+	private Cliente cliente;
+	
+	public Os() {
+		super();
+		this.setDataAbertura(LocalDateTime.now());
+		this.setPrioridade(Prioridade.BAIXA);
+		this.setStatus(Status.ABERTO);
+	}
+
+	public Os(Integer id, Prioridade prioridade, String observacoes, Status status, Tecnico tecnico, Cliente cliente) {
+		super();
+		this.id = id;
+		this.setDataAbertura(LocalDateTime.now());
+		this.dataFechamento = dataFechamento;
+		this.prioridade = (prioridade == null) ? 0 : prioridade.getKey();
+		this.observacoes = observacoes;
+		this.status = (status == null) ? 0 : status.getKey();
+		this.tecnico = tecnico;
+		this.cliente = cliente;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(LocalDateTime dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+
+	public LocalDateTime getDataFechamento() {
+		return dataFechamento;
+	}
+
+	public void setDataFechamento(LocalDateTime dataFechamento) {
+		this.dataFechamento = dataFechamento;
+	}
+
+	public Prioridade getPrioridade() {
+		return Prioridade.toEnum(this.prioridade);
+	}
+
+	public void setPrioridade(Prioridade prioridade) {
+		this.prioridade = prioridade.getKey();
+	}
+
+	public String getObservacoes() {
+		return observacoes;
+	}
+
+	public void setObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+	}
+
+	public Status getStatus() {
+		return Status.toEnum(this.status);
+	}
+
+	public void setStatus(Status status) {
+		this.status = status.getKey();
+	}
+
+	public Tecnico getTecnico() {
+		return tecnico;
+	}
+
+	public void setTecnico(Tecnico tecnico) {
+		this.tecnico = tecnico;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cliente, dataAbertura, dataFechamento, id, observacoes, prioridade, status, tecnico);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Os other = (Os) obj;
+		return Objects.equals(cliente, other.cliente) && Objects.equals(dataAbertura, other.dataAbertura)
+				&& Objects.equals(dataFechamento, other.dataFechamento) && Objects.equals(id, other.id)
+				&& Objects.equals(observacoes, other.observacoes) && prioridade == other.prioridade
+				&& Objects.equals(status, other.status) && Objects.equals(tecnico, other.tecnico);
+	}
+	
+}
