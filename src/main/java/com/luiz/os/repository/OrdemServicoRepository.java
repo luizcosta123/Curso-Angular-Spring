cdfa9bbd1117469994539f7c52b466c17d034228
luizc <luizc@DESKTOP-PLL2PIG>
2022-09-27 10:12:42 -0300

    Created OrdemServicoRepository

--- src/main/java/com/luiz/os/repository/OrdemServicoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.luiz.os.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.luiz.os.model.OrdemServico;
-
-public interface OrdemServicoRepository extends JpaRepository<OrdemServico, Integer> {
-
-}
