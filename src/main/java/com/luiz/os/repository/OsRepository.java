60f21768b933bbd5e2eb8e47496d1fa5f416832e
luizc <luizc@DESKTOP-PLL2PIG>
2022-09-27 10:11:21 -0300

    Created OsRepository

---
+++ src/main/java/com/luiz/os/repository/OsRepository.java
@@ -0,0 +1,9 @@
+package com.luiz.os.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luiz.os.model.Os;
+
+public interface OsRepository extends JpaRepository<Os, Integer> {
+
+}
