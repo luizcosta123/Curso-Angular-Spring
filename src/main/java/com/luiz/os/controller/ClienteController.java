0e32bbde7e5ca69788faa5a3ea328116c71f3879
luizc <luizc@DESKTOP-PLL2PIG>
2022-09-27 09:58:04 -0300

    Created ClienteController

--- src/main/java/com/luiz/os/controller/ClienteController.java
+++ src/main/java/com/luiz/os/controller/ClienteController.java
@@ -1,15 +1,30 @@
 package com.luiz.os.controller;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.luiz.os.dto.ClienteDto;
+import com.luiz.os.model.Cliente;
 import com.luiz.os.service.ClienteService;
 
+@CrossOrigin("*")
 @RestController()
 @RequestMapping("/clientes")
 public class ClienteController {
@@ -23,5 +38,52 @@ public class ClienteController {
 		
 		return ResponseEntity.ok().body(clienteDto);
 	}
+	
+	@GetMapping()
+	public ResponseEntity<List<ClienteDto>> findAll() {
+		List<ClienteDto> clienteDtoList = clienteService.findAll().stream().map(cliente -> new ClienteDto(cliente)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(clienteDtoList);
+	}
+	
+	@PostMapping()
+	public ResponseEntity<ClienteDto> create(@Valid @RequestBody ClienteDto clienteDto) {
+		Cliente cliente = clienteService.create(clienteDto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cliente.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<ClienteDto> update(@PathVariable Integer id, @Valid @RequestBody ClienteDto clienteDto) {
+		ClienteDto newClienteDto = new ClienteDto(clienteService.update(id, clienteDto));
+		
+		return ResponseEntity.ok().body(newClienteDto);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		clienteService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
